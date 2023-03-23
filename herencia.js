e12f9efa8ac6c61006949db5a7381b66abdfe834
sebastian <sebasr1268@gmail.com>
2023-03-23 09:43:41 -0500

    añadiendo dias restantes para el cumpleaños

--- herencia.js
+++ herencia.js
@@ -2,31 +2,20 @@ let nombre=document.querySelector('.nombre')
 let felizCumple=document.querySelector('.felizCumple')
 datos=JSON.parse(localStorage.getItem('misdatos'))
 let indice=JSON.parse(localStorage.getItem('indice'))
+const devolver=document.getElementById('devolver')
+
 let hoy2=[]
 let fechas=[]
 
 let hoy = new Date();
 
-hoy2[0] = new Date().getMonth();
+hoy2[0] = new Date().getMonth()+1;
 hoy2[1] = new Date().getDate();
 fechas=datos.fecha[indice].split('-')
-console.log(hoy2)
-console.log(fechas)
+
 let fecha3=datos.fecha[indice].split('-')
 fecha3[0]=2023
-const cumple = new Date(hoy.getFullYear(),fecha3[1]-1,fecha3[2] );
-console.log(cumple)
-console.log(hoy)
 
-const diff =Math.abs( (cumple - hoy) /(1000 * 60 * 60 * 24))
-
-console.log(diff)
-let result=`Faltan ${Math.round(diff)} días para tu cumpleaños`;
-
-
-
-console.log(datos)
-console.log(indice)
 
 let div1=document.createElement('div')
 div1.textContent=datos.nombres[indice]
@@ -38,9 +27,36 @@ if(datos.felizC[indice]=='si'){
     felizCumple.append(div2)
 
 }else{
-    if(parseInt(hoy2[0])<parseInt(fechas[1]) || parseInt(hoy2[1])<parseInt(fechas[2])){
-    div2.textContent=result
-    felizCumple.append(div2)
+    if(parseInt(hoy2[0])<=parseInt(fechas[1])){
+        if( parseInt(hoy2[1])<=parseInt(fechas[2])){
+            const cumple = new Date(hoy.getFullYear(),fecha3[1]-1,fecha3[2] );
+            const diff =Math.abs( (cumple - hoy) /(1000 * 60 * 60 * 24))
+            let result=`Faltan ${Math.round(diff)} días para tu cumpleaños`;
+            
+            div2.textContent=result
+            felizCumple.append(div2)
+        }
+ 
+    }if(parseInt(hoy2[0])<parseInt(fechas[1]) && parseInt(hoy2[1])>parseInt(fechas[2])){
+        const cumple = new Date(hoy.getFullYear(),fecha3[1]-1,fecha3[2] );
+        const diff =Math.abs( (cumple - hoy) /(1000 * 60 * 60 * 24))
+        let result=`Faltan ${Math.round(diff)} días para tu cumpleaños`;
+        
+        div2.textContent=result
+        felizCumple.append(div2)
+    }
+    if(parseInt(hoy2[0])>=parseInt(fechas[1])){
+        if( parseInt(hoy2[1])<parseInt(fechas[2]) || parseInt(hoy2[1])>parseInt(fechas[2])){
+            const cumple = new Date(hoy.getFullYear()+1,fecha3[1]-1,fecha3[2] );
+            const diff =Math.abs( (cumple - hoy) /(1000 * 60 * 60 * 24))
+            let result=`Faltan ${Math.round(diff)} días para tu cumpleaños`;
+    
+            div2.textContent=result
+            felizCumple.append(div2)
+        }
+
     }
 }
-
+devolver.addEventListener('click',()=>{
+    location.href='./index.html'
+})
